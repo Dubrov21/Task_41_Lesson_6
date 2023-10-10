63df6f22a748c5e3145aad68b5b528ef8f51333f
Dubrov21 <vald5red@yandex.ru>
2023-10-10 20:24:01 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+using System;
+
+namespace Task_41_Lesson_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int count = 0;
+            int number = 0;
+            while (true)
+            {
+                Console.WriteLine("Введите целое число или любую букву для окончания ввода");
+                if(int.TryParse(Console.ReadLine(), out number))
+                {
+                    if (number > 0)
+                    {
+                        count++;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Количество введённых положительных чисел равно {count}");
+                    break;
+                }
+            }
+            
+
+        }
+    }
+}
